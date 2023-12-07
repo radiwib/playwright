04bc9bb14617b9724215d4d13313f8d4e6285b60
radiwib <radi.wib@gmail.com>
2023-12-07 18:11:02 +0700

    RWB_oko

--- tests/soucelabs_test1.spec.js
+++ tests/soucelabs_test1.spec.js
@@ -4,5 +4,5 @@ test('check title', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
   
     // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Swag Labs/);
+    await expect(page).toHaveTitle('Swag Labs');
   });

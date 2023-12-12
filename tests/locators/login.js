832a488cd20a631c38f640c93b9b4de9e05cac31
radiwib <radi.wib@gmail.com>
2023-12-12 10:41:36 +0700

    commit

---
+++ tests/locators/login.js
@@ -0,0 +1,9 @@
+import {test} from '../tests/@playwright/test';
+
+exports.loginLocators = function LL (){
+    
+    const username = page.locator('[data-test="username"]');
+    const password = page.locator('');
+    const loginButton = page.locator('');
+
+}
